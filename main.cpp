50190734a06cf629fcfb411bbf2ebe9697ebfcb2
nmv13 <vel16002@byui.edu>
2020-04-25 17:19:21 -0600

    comments added

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 /*************************************************************
- * This is the full chess program in C++
+ * This is a full chess program in C++. It initiates a board
+ * and allows users to play chess with error/rule checking
+ * capabilities.
  ************************************************************/
 
 #include <iomanip>
@@ -15,7 +17,11 @@ char convertLetter(char let);
 int convertNumber(char num);
 int convertNumberInverse(char num);
 
-
+/*************************************************************
+ * This function initiates all required components of the 
+ * chess game, then calls the options() method where the 
+ * program will run until it is terminated.
+ ************************************************************/
 int main()
 {
 	// Create and display board with starting peace positions
@@ -23,7 +29,7 @@ int main()
     initialBoard(board);
     display(board);
 
-    //Display main menu and start game
+    // Display main menu and start game
     cout << "Options:\n"
 	   	 << "   ? Show options\n"
 	   	 << "   D Display the board\n"
@@ -33,20 +39,29 @@ int main()
 
 	cout << "Please enter a command: ";
 
+	// GO to options function to initiate game loop
     options(board);
 
     return 0;
 }
 
+/*************************************************************
+ * This function reads in the user input for each turn and 
+ * executes their command.
+ ************************************************************/
 bool options(Piece board[][8])
 {
+	// Create answer variable to store user input
 	char ans;
 
 	// Display main menu and start game
 	while (1)
 	{
+		// Read in user input
 		cin >> ans;
 
+		// Begin if statement
+		// If input is "?", display options again
 		if (ans == '?')
 		{
 			cout << "\nOptions:\n"
@@ -57,16 +72,19 @@ bool options(Piece board[][8])
 	    	 << endl
 	    	 << "Please enter a command: ";
 		}
+		// If input is "D", display the current board
 		else if (ans == 'D' || ans == 'd')
 		{
 			display(board);
 			cout << "Please enter a command: ";
 		}
+		// If input is "M", call the move() function
 		else if (ans == 'M' || ans == 'm')
 		{
 			move(board);
 			cout << "Please enter a command: ";
 		}
+		// If input is "E", verify the user wants to end the game, then end the game, otherwise continue
 		else if (ans == 'E' || ans == 'e')
 		{
 			char temp;
@@ -93,11 +111,17 @@ bool options(Piece board[][8])
 	}
 }
 
+/*************************************************************
+ * This function initiates the board pieces before the game
+ * starts. This happens before options() is initially called.
+ ************************************************************/
 void initialBoard(Piece board[][8])
 {
+	// Initiate all starter pieces for the board
 
 	Space e;  // Empty square
 
+	// All pieces for White side
 	Pawn p00; p00.setColor(0);
     Pawn p01; p01.setColor(0);
     Pawn p02; p02.setColor(0);
@@ -121,6 +145,7 @@ void initialBoard(Piece board[][8])
 
     //////////////////////////
 
+    // All pieces for black side
     Pawn p10; p10.setColor(1);
     Pawn p11; p11.setColor(1);
     Pawn p12; p12.setColor(1);
@@ -142,6 +167,7 @@ void initialBoard(Piece board[][8])
     Queen q10; q10.setColor(1);
     King k10; k10.setColor(1);
 
+    // Possiblly add color later on
 	/*HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
  	// you can loop k higher to see more color choices
   	for(int k = 1; k < 255; k++)
@@ -151,8 +177,10 @@ void initialBoard(Piece board[][8])
     	cout << k << " I want to be nice today!" << endl;
   	}*/
 
-	// Top rows of board
+    // Put initialized pieces on the board in a specified order according to the piece
+    // to reflect a standard chess board
 
+	// Top rows of board
 	// Collumn 8
 	board[0][0] = r10;
 	board[0][1] = n10;
@@ -174,7 +202,6 @@ void initialBoard(Piece board[][8])
 	board[1][7] = p17;
 
 	// Bottom rows of board
-
 	// Collumn 2
 	board[6][0] = p00;
 	board[6][1] = p01;
@@ -195,6 +222,7 @@ void initialBoard(Piece board[][8])
 	board[7][6] = n01;
 	board[7][7] = r01;
 
+	// All remaining squares on the board must have an emply space represented by the Space object 
 	for (int i = 2; i < 6; ++i)
 	{
 		for (int j = 0; j < 8; ++j)
@@ -204,25 +232,42 @@ void initialBoard(Piece board[][8])
 	}
 }
 
+/*************************************************************
+ * This function reads in the moves of the user, makes checks 
+ * on the validity of the move, the performs the move by 
+ * applying it to the board array.
+ ************************************************************/
 void move(Piece board[][8])
 {
+	// Initialize variables for the user input, then variables for converting the inputs
 	char fLet,fNum,tLet,tNum,ph; // from letter, from number, placeholder, to letter, to number
 	int fl,fn,tl,tn;             // abbreviated variables translated to intigers
+
+	// Use check variable to break from loop when a valid input is entered by the user.
 	bool check = false;
+
+	// Use Space to fill the slot that a piece moved from.
 	Space e;
 
+	// Keep looping until user has entered a valid move
 	while (check != true)
 	{
+		// Read in inputs from user in specified format (b2:b4) representing where a piece is 
+		// moving from, and where it is moving to.
 		cout << "\nEnter your move (b2:b4): ";
 		cin  >> fLet >> fNum >> ph >> tLet >> tNum;
 
+		// Convert char letters into char numbers
 		fLet = convertLetter(fLet);
 		tLet = convertLetter(tLet);
 		
+		// Convert all char numbers into int numbers so the board index can read it
 		fl = convertNumber(fLet);
 		fn = convertNumberInverse(fNum);
 		tl = convertNumber(tLet);
 		tn = convertNumberInverse(tNum);
+
+		// Check to see if a piece specified by the user is able to move to the inputted coordinates 
 
 		// cout << fl << " " << fn << " " << tl << " " << tn << endl;board[fn][fl].canMove(fn, fl, tn, tl)
 		// cout << board[fn][fl].color << endl;
@@ -234,12 +279,19 @@ void move(Piece board[][8])
 		//board[1][7].setColor(1);
 	}
 
+	// Apply piece location change to the board while replacing the voided slot with the Space object
 	board[tn][tl] = board[fn][fl];
 	board[fn][fl] = e;
 
+	// Display the board to the user for move confirmation
 	display(board);
 }
 
+/*************************************************************
+ * This function displays the board to the user. This is done
+ * by using a template board and displaying each individual 
+ * piece of the array where appropriate according to index.
+ ************************************************************/
 char display(Piece board[][8])
 {
 	// Board compenents
@@ -247,11 +299,13 @@ char display(Piece board[][8])
 	string bb = "----";  // Boarder
 	string ss = "|";     // Square Start
 
+	// Horizontal line separating rows on board
 	string boarder = "   " + bs + bb + bb + bb + bb + bb + bb + bb + bb + "\n";
 
-	// Board and pieces
+	// Display board while adding pieces where applicable
 	for (int i = 0; i < 8; ++i)
 	{
+		// Add numeric representation of vertical coordinates at the start of each row
 		cout << endl << boarder << " " << 8 - i << " " << ss;
 		for (int j = 0; j < 8; ++j)
 		{
@@ -261,6 +315,7 @@ char display(Piece board[][8])
 		}
 	}
 
+	// Add letter representation of horizontal coordinates at the bottom of the board
 	cout << endl << boarder;
 	cout << "     A   B   C   D   E   F   G   H\n\n";
 }
@@ -295,14 +350,15 @@ char convertLetter(char let)
 }
 
 /**********************************************************************
- * This function converts char numbers into int numbers for display.
+ * This function converts char numbers into int numbers in reverse for 
+ * display.
  *********************************************************************/
 int convertNumberInverse(char num)
 {
    // Declare number variable for return
    int number;
 
-   // Translate char number to int number
+   // Translate char number to int number reversed
    if (num == '1')
       number = 7;
    if (num == '2')
--- piece.h
+++ piece.h
@@ -7,11 +7,15 @@
 #include <iostream>
 using namespace std;
 
+/*************************************************************
+ * This class establishes a piece object with all basic 
+ * components.
+ ************************************************************/
 class Piece
 {
    protected:
-      bool alive;
-      bool first;
+      bool alive;                                // States whether object is alive
+      bool first;                                // States whether it is that piece's first move
 
    public:
       Piece()
@@ -22,14 +26,25 @@ class Piece
       char color;                                // Holds display piece
       void setColor(int col);                    // Set piece color
       bool isAlive() { return alive; }           // Check to see if piece is alive
-      int canMove(int a, int b, int c, int d);   // Check if piece can be moved
+
+      // This function checks where a given piece can move on a chess board in any situation. This 
+      // happens by reading in the coordinates of the piece. It does not matter where the piece is 
+      // on the board at the time, this function only specifies if the move is allowed or not in general.
+      // Other functions will take care of the rest of the work. Essentially, this function reads in 4 numbers 
+      // and applies them as if they were on a graph, if the first two (x,y) numbers in relation to the second
+      // pair of (x,y) is a valid move from point 1 to point 2 according to the chess piece, then it returns true. 
+      int canMove(int a, int b, int c, int d);
 };
 
+/*************************************************************
+ * This child class is specific to a Pawn in a chess game.
+ ************************************************************/
 class Pawn : public Piece
 {
    private:
 
    public:
+      // Color in this case represents the piece's char case
       void setColor(int col)
       {
          if (col == 0)
@@ -38,24 +53,29 @@ class Pawn : public Piece
             color = 'p';
       }
 
+      // This function establishes where a Pawn can move in general situations
       int canMove(int a, int b, int c, int d) 
       {  
+         // If it is this Pawn's first move of the game, it can move forward 2 squares
          if (a == c && (b + 2) == d)
             if (first)
             {
                first = false;
                return true;
             }
+         // The Pawn can move forward 1 space
          else if (a == c && (b + 1) == d)
          {
             first = false;
             return true;
          }
+         // The Pawn can move one square forward and one square right
          else if ((a + 1) == c && (b + 1) == d)
          {
             first = false;
             return true;
          }
+         // The Pawn can move one square forward and one square left
          else if ((a - 1) == c && (b - 1) == d)
          {
             first = false;
@@ -66,11 +86,15 @@ class Pawn : public Piece
       }
 };
 
+/*************************************************************
+ * This child class is specific to a Rook in a chess game.
+ ************************************************************/
 class Rook : public Piece
 {
    private:
 
    public:
+      // Color in this case represents the piece's char case
       void setColor(int col)
       {
          if (col == 0)
@@ -79,10 +103,13 @@ class Rook : public Piece
             color = 'r';
       }
 
+      // This function establishes where a Rook can move in general situations
       int canMove(int a, int b, int c, int d) 
       {
+         // The Rook can move vertically in either direction
          if (a == c && b != d)
             return true;
+         // The ROok can move horizontally in either direction
          else if (a != c && b == d)
             return true;
          else
@@ -90,11 +117,15 @@ class Rook : public Piece
       }
 };
 
+/*************************************************************
+ * This child class is specific to a Knight in a chess game.
+ ************************************************************/
 class Knight : public Piece
 {
    private:
 
    public:
+      // Color in this case represents the piece's char case
       void setColor(int col)
       {
          if (col == 0)
@@ -103,18 +134,22 @@ class Knight : public Piece
             color = 'n';
       }
 
+      // This function establishes where a Knight can move in general situations
       int canMove(int a, int b, int c, int d) 
       {
+         // The Knight can move forward two spaces and to the right or the left one space
          if ((a + 2) == c)
          {
             if ((b + 1) == d || (b - 1) == d)
                return true;
          }
+         // The Knight can move backward two spaces and to the right or the left one space
          else if ((a - 2) == c)
          {
             if ((b + 2) == d || (b - 2) == d)
                return true;      
          }
+         // The Knight can move forward or backward one space and to the left or right two spaces
          else if ((a + 1) == d || (a - 1) == d)
          {
             if ((b + 2) == d || (b - 2) == d)
@@ -125,11 +160,15 @@ class Knight : public Piece
       }
 };
 
+/*************************************************************
+ * This child class is specific to a Bishop in a chess game.
+ ************************************************************/
 class Bishop : public Piece
 {
    private:
 
    public:
+      // Color in this case represents the piece's char case
       void setColor(int col)
       {
          if (col == 0)
@@ -138,8 +177,10 @@ class Bishop : public Piece
             color = 'b';
       }
 
+      // This function establishes where a Bishop can move in general situations
       int canMove(int a, int b, int c, int d) 
       {
+         // The Bishop can move diagonally in any direction
          if ((c - a) == (d - b))
             return true;
          else if ((c + a) == (d + b))
@@ -149,11 +190,15 @@ class Bishop : public Piece
       }
 };
 
+/*************************************************************
+ * This child class is specific to a Queen in a chess game.
+ ************************************************************/
 class Queen : public Piece
 {
    private:
 
    public:
+      // Color in this case represents the piece's char case
       void setColor(int col)
       {
          if (col == 0)
@@ -162,14 +207,19 @@ class Queen : public Piece
             color = 'q';
       }
 
+      // This function establishes where a Queen can move in any situation
       int canMove(int a, int b, int c, int d) 
       {
+         // The Queen can move disgonally left in either direction
          if ((c - a) == (d - b))
             return true;
+         // The Queen can move disgonally right in either direction
          else if ((c + a) == (d + b))
             return true;
+         // The Queen can move vertically in either direction
          else if (a == c && b != d)
             return true;
+         // The Queen can move horizontally in either direction
          else if (a != c && b == d)
             return true;
          else
@@ -177,11 +227,15 @@ class Queen : public Piece
       }
 };
 
+/************************************************************
+ * This child class is specific to a King in a chess game.
+ ***********************************************************/
 class King : public Piece
 {
    private:
 
    public:
+      // Color in this case represents the piece's char case
       void setColor(int col)
       {
          if (col == 0)
@@ -190,13 +244,16 @@ class King : public Piece
             color = 'k';
       }
 
+      // This function establishes where a King can move in any situation
       int canMove(int a, int b, int c, int d) 
       {
+         // The King can move forward or backward one square in any direction
          if ((a + 1) == c || (a - 1) == c || a == c)
          {
             if ((b + 1) == d || (b - 1) == d)
                return true;
          }
+         // The King can move one square to the right or left
          else if (b == d)
          {
             if ((a + 1) == c || (a - 1) == c)
@@ -207,14 +264,19 @@ class King : public Piece
       }
 };
 
+/*************************************************************
+ * This child class takes the place of any square on the 
+ * board that is not populated by a chess piece.
+ ************************************************************/
 class Space : public Piece
 {
    public:
       Space()
       {
-         color = ' ';
+         color = ' ';    // For this class, the color is a space representing an empty square
       }
 
+      // A space can't move
       int canMove(int a, int b, int c, int d)
       {
          return false;
